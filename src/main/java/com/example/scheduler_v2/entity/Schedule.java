5f12c0f84bd71e4a094d9062f98c8767d581a743
gyoungsubLee <dltjq2323@gmail.com>
2024-11-15 07:52:26 +0900

    feat: Schedule Entity

---
+++ src/main/java/com/example/scheduler_v2/entity/Schedule.java
@@ -0,0 +1,26 @@
+package com.example.scheduler_v2.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor
+public class Schedule extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "schedule_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    private String descriptiom;
+
+    public Schedule(String title, String descriptiom) {
+        this.title = title;
+        this.descriptiom = descriptiom;
+    }
+}
