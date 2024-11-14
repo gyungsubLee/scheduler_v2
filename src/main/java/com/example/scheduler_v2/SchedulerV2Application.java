6f920da55877d58616481d5c3f498427840650ea
gyoungsubLee <dltjq2323@gmail.com>
2024-11-15 01:03:56 +0900

    project init

---
+++ src/main/java/com/example/scheduler_v2/SchedulerV2Application.java
@@ -0,0 +1,13 @@
+package com.example.scheduler_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulerV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchedulerV2Application.class, args);
+    }
+
+}
