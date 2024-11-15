7d748d8b0f0924ec6b4c11e0247385ed7d78a62b
gyoungsubLee <dltjq2323@gmail.com>
2024-11-15 20:29:05 +0900

    feat: LoginFilter, WebConfig • 세션 값으로 로그인 상태 확인 • 제외 url -  "/members", "/member/login"

---
+++ src/main/java/com/example/scheduler_v2/config/WebConfig.java
@@ -0,0 +1,21 @@
+package com.example.scheduler_v2.config;
+
+import com.example.scheduler_v2.filter.LoginFilter;
+import jakarta.servlet.Filter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public FilterRegistrationBean loginFilter() {
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new LoginFilter());
+        filterRegistrationBean.setOrder(1);
+        filterRegistrationBean.addUrlPatterns("/*");
+
+        return filterRegistrationBean;
+    }
+}
